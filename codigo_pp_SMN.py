743428793e2631c65ce482284b74ed299c11b3ca
Nadia Testani <nadiatestani@gmail.com>
2023-05-25 16:42:10 +0300

    cambie una cosita mas desde spyder

--- codigo_pp_SMN.py
+++ codigo_pp_SMN.py
@@ -22,7 +22,7 @@ sbn.set() # Setting seaborn as default style even if use only matplotlib
 
 #estamos probando git
 #cambio una cosita mas
-
+#cambio una cosita mas desde spyder
 #%%
 directorio = 'data_set'
 archivo= 'ppSMN.csv'
